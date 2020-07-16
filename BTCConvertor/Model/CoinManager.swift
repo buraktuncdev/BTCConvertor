f4763bfbd143ef1ab9c034405aa84d6d0e8885d9
Burak Tunç <38864136+buraktuncdev@users.noreply.github.com>
2020-07-16 16:18:10 +0300

    Update CoinManager.swift

--- BTCConvertor/Model/CoinManager.swift
+++ BTCConvertor/Model/CoinManager.swift
@@ -11,7 +11,7 @@ protocol CoinManagerDelegate {
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "7D39FF23-257D-4793-A1BE-CF2DC6EE5164"
+    let apiKey = "<API_KEY>"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
